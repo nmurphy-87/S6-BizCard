30f6e08889f739f438b93473c1d0b4b7779b5f26
Niall <niall@Aarons-MacBook-Air.local>
2023-11-24 16:43:33 +0000

    added profile icon and divider

--- app/src/main/java/com/niallmurph/s6bizcard/MainActivity.kt
+++ app/src/main/java/com/niallmurph/s6bizcard/MainActivity.kt
@@ -3,17 +3,21 @@ package com.niallmurph.s6bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.niallmurph.s6bizcard.ui.theme.S6BizCardTheme
@@ -38,11 +42,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CreateBizCard(
 
-){
+) {
     Surface(
         modifier = Modifier
             .fillMaxSize()
-    ){
+    ) {
         Card(
             modifier = Modifier
                 .width(200.dp)
@@ -50,9 +54,38 @@ fun CreateBizCard(
                 .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = 5.dp
-        ){
+        ) {
+
+            Column(
+                modifier = Modifier.height(height = 300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider(thickness = 2.dp)
+            }
+
 
         }
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier : Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(1.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_user_profile),
+            contentDescription = "Profile placeholder pic"
+        )
     }
 }
 
